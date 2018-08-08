d5149f81fb6b0beac5acb02461f1db5ae0a9b5ff
mozgbrasil <mozgbrasil@gmail.com>
2018-08-08 18:08:30 -0300

    .

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/hello', (req, res) => res.send('Hello World!'))
+  // /files/* is accessed via req.params[0]
+  // but here we name it :file
+  .get('/files/:file(*)', function(req, res, next){
+    var filePath = path.join(__dirname, 'files', req.params.file);
+
+    res.download(filePath, function (err) {
+      if (!err) return; // file sent
+      if (err && err.status !== 404) return next(err); // non-404 error
+      // file for download not found
+      res.statusCode = 404;
+      res.send('Cant find that file, sorry!');
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
